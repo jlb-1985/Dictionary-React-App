210984af37da337790f142ff4dabc633393c2cdd
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2026-05-18 21:38:03 +0200

    Integrated Shecodes Dictionary API

--- src/SearchEngine.jsx
+++ src/SearchEngine.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import axios from "axios"
+import axios from "axios";
 
 export default function SearchEngine() {
   const [keyword, setKeyword] = useState("");
   const [result, setResult] = useState("");
 
+  function handleResponse(response) {
+    // SheCodes API geeft: response.data.word
+    setResult(response.data.word);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    setResult(keyword);
+
+    let apiKey = "135f02bffcta720ff4c241394b466o99"
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
@@ -30,7 +39,9 @@ export default function SearchEngine() {
       {result && (
         <div className="card p-4 shadow-sm result-card">
           <h2 className="result-title">{result}</h2>
-          <p className="text-muted fst-italic"></p>
+          <p className="text-muted fst-italic">
+            (meer info komt zodra we meanings toevoegen)
+          </p>
         </div>
       )}
     </div>
