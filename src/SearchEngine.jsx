c52e6c6e4ed090bcf3998fff3bf97a5dcacb0218
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2026-05-19 15:23:22 +0200

    Update SearchEngine.jsx

--- src/SearchEngine.jsx
+++ src/SearchEngine.jsx
@@ -4,12 +4,14 @@ import axios from "axios";
 
 export default function SearchEngine() {
   const [keyword, setKeyword] = useState("");
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(null);
+
 
   function handleResponse(response) {
     // Free Dictionary API geeft een array terug
     // response.data[0].word = het woord
-    setResult(response.data[0].word);
+    setResult(response.data[0]);
+
   }
 
   function handleSubmit(event) {
@@ -38,14 +40,36 @@ export default function SearchEngine() {
         </div>
       </form>
 
-      {result && (
-        <div className="card p-4 shadow-sm result-card">
-          <h2 className="result-title">{result}</h2>
-          <p className="text-muted fst-italic">
-            (meer info komt zodra we meanings tonen)
-          </p>
-        </div>
+     {result && (
+  <div className="card p-4 shadow-sm result-card">
+    <h2 className="result-title">{result.word}</h2>
+
+    {/* Fonetiek */}
+    {result.phonetics && result.phonetics[0] && (
+      <p className="text-muted">
+        {result.phonetics[0].text}
+      </p>
+    )}
+
+    {/* Definitie */}
+    {result.meanings && result.meanings[0] && (
+      <p>
+        {result.meanings[0].definitions[0].definition}
+      </p>
+    )}
+
+    {/* Synoniemen */}
+    {result.meanings[0].synonyms &&
+      result.meanings[0].synonyms.length > 0 && (
+        <p className="mt-3">
+          <strong>Synonyms:</strong> {result.meanings[0].synonyms.join(", ")}
+        </p>
       )}
+  </div>
+)}
     </div>
   );
 }
+
+
+
